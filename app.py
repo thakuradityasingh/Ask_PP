ade63f56e2486e6dd9a4aeefeab57099871e9dbe
thakuradityasingh <ast1b2910@gmail.com>
2023-06-11 23:36:30 +0530

    intial commit

---
+++ app.py
@@ -0,0 +1,69 @@
+import streamlit as st
+from dotenv import load_dotenv
+import pickle
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.callbacks import get_openai_callback
+import os
+
+# Load environment variables
+load_dotenv()
+
+# Define the main function
+def main():
+    st.header("Ask PP 💬")
+
+    # Upload a PDF file
+    pdf = st.file_uploader("Upload your PDF", type='pdf')
+
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+        )
+        chunks = text_splitter.split_text(text=text)
+
+        store_name = os.path.splitext(pdf.name)[0]
+        st.write(f'{store_name}')
+
+        if os.path.exists(f"{store_name}.pkl"):
+            with open(f"{store_name}.pkl", "rb") as f:
+                VectorStore = pickle.load(f)
+        else:
+            embeddings = OpenAIEmbeddings()
+            VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+            with open(f"{store_name}.pkl", "wb") as f:
+                pickle.dump(VectorStore, f)
+
+        query = st.text_input("Ask questions:")
+
+        if query:
+           
+            variations = []
+            for word in query.split():
+                variations.extend([word, word.capitalize(), word.lower()])
+            query = " ".join(variations)
+
+            docs = VectorStore.similarity_search(query=query, k=3)
+
+            llm = OpenAI(temperature=.7)
+            chain = load_qa_chain(llm=llm, chain_type="stuff")
+            with get_openai_callback() as cb:
+                response = chain.run(input_documents=docs, question=query)
+                st.write(cb)
+            st.write(response)
+
+# Run the main function if this script is executed directly
+if __name__ == '__main__':
+    main()
